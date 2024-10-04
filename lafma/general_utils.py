451e8a17af428f5fac83d105e9252f9b67c928a9
Srikar Kavirayuni <84162099+SrikarK-code@users.noreply.github.com>
2024-10-03 22:02:16 -0700

    Update general_utils.py

--- lafma/general_utils.py
+++ lafma/general_utils.py
@@ -159,12 +159,10 @@ class TimestepBlock(nn.Module):
         """
 
 class TimestepEmbedSequential(nn.Sequential, TimestepBlock):
-    def forward(self, x, emb, context=None):
+    def forward(self, x, emb):
         for layer in self:
             if isinstance(layer, TimestepBlock):
                 x = layer(x, emb)
-            elif isinstance(layer, SpatialTransformer):
-                x = layer(x, context)
             else:
                 x = layer(x)
         return x
