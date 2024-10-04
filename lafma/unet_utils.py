ba43a774c00690856a7bb3af9e77ecca1e9fe944
Srikar Kavirayuni <84162099+SrikarK-code@users.noreply.github.com>
2024-10-03 22:01:05 -0700

    Update unet_utils.py

--- lafma/unet_utils.py
+++ lafma/unet_utils.py
@@ -251,7 +251,7 @@ class QKVAttention(nn.Module):
         return a.reshape(bs, -1, length)
 
 class SpatialTransformer(nn.Module):
-    def __init__(self, in_channels, n_heads, d_head, depth=1, dropout=0., context_dim=None):
+    def __init__(self, in_channels, n_heads, d_head, depth=1, dropout=0.):
         super().__init__()
         self.in_channels = in_channels
         inner_dim = n_heads * d_head
@@ -260,42 +260,38 @@ class SpatialTransformer(nn.Module):
         self.proj_in = nn.Conv1d(in_channels, inner_dim, kernel_size=1, stride=1, padding=0)
 
         self.transformer_blocks = nn.ModuleList(
-            [BasicTransformerBlock(inner_dim, n_heads, d_head, dropout=dropout, context_dim=context_dim)
+            [BasicTransformerBlock(inner_dim, n_heads, d_head, dropout=dropout)
              for d in range(depth)]
         )
 
         self.proj_out = zero_module(nn.Conv1d(inner_dim, in_channels, kernel_size=1, stride=1, padding=0))
 
-    def forward(self, x, context=None):
-        # note: if no context is given, cross-attention defaults to self-attention
+    def forward(self, x):
         b, c, s = x.shape
         x_in = x
         x = self.norm(x)
         x = self.proj_in(x)
         x = rearrange(x, 'b c s -> b s c')
         for block in self.transformer_blocks:
-            x = block(x, context=context)
+            x = block(x)
         x = rearrange(x, 'b s c -> b c s')
         x = self.proj_out(x)
         return x + x_in
 
 class BasicTransformerBlock(nn.Module):
-    def __init__(self, dim, n_heads, d_head, dropout=0., context_dim=None, gated_ff=True, checkpoint=True):
+    def __init__(self, dim, n_heads, d_head, dropout=0., gated_ff=True, checkpoint=True):
         super().__init__()
         self.attn1 = CrossAttention(query_dim=dim, heads=n_heads, dim_head=d_head, dropout=dropout)
         self.ff = FeedForward(dim, dropout=dropout, glu=gated_ff)
-        self.attn2 = CrossAttention(query_dim=dim, context_dim=context_dim,
-                                    heads=n_heads, dim_head=d_head, dropout=dropout)
         self.norm1 = nn.LayerNorm(dim)
         self.norm2 = nn.LayerNorm(dim)
-        self.norm3 = nn.LayerNorm(dim)
         self.checkpoint = checkpoint
 
-    def forward(self, x, context=None):
-        return checkpoint(self._forward, (x, context), self.parameters(), self.checkpoint)
+    def forward(self, x):
+        return checkpoint(self._forward, (x,), self.parameters(), self.checkpoint)
 
-    def _forward(self, x, context=None):
+    def _forward(self, x):
         x = self.attn1(self.norm1(x)) + x
-        x = self.attn2(self.norm2(x), context=context) + x
-        x = self.ff(self.norm3(x)) + x
+        x = self.ff(self.norm2(x)) + x
         return x
+
